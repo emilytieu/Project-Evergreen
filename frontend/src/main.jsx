9f161a93bab0281c6a0b993001a374f1c54f2654
emilytieu <emily.tieu@mail.utoronto.ca>
2026-06-25 21:07:31 -0500

    linked predictor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import HydrogenBuilder from "./HydrogenBuilder";
+import Predictor from "./Predictor";
 import "./builder.css";
+import "./predictor.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HydrogenBuilder />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HydrogenBuilder />} />
+        <Route path="/predictor" element={<Predictor />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
